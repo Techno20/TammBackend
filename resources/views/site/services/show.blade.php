057e715dd05d15ac9ddf27fc013fddba355e050c
palgooal <info@palgoal.ps>
2021-02-24 00:53:39 +0200

    show pop

--- resources/views/site/services/show.blade.php
+++ resources/views/site/services/show.blade.php
@@ -371,6 +371,96 @@
         @endif
     </div>
 
+     <!-- send-message modal -->
+     <div class="modal fade custom-modal send-message-modal" id="sendMessageModal" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered ">
+            <div class="modal-content">
+                <header class="head d-flex justify-content-between">
+                    <h3>{{__('site.contact_seller')}}</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </header>
+                <div class="content">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="details">
+                                <div class="user-box">
+                                    <div class="user media">
+                                        <figure>
+                                            <img src="{{ asset('assets/site/images/user.png') }}" alt="">
+                                            {{-- <span></span> --}}
+                                        </figure>
+                                        <div class="media-body">
+                                            <h4>{{ $service->user->name }}</h4>
+                                            {{-- <p>Online</p> --}}
+                                        </div>
+                                    </div>
+                                    {{-- <div class="times">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="time">
+                                                    <label>Local Time</label>
+                                                    <p>Mon 18:02</p>   
+                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <div class="time">
+                                                    <label>Avg. Rspns</label>
+                                                    <p>Mon 18:02</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div> --}}
+                                </div>
+                                <div class="includes-box">
+                                    <p>زودانا بمعلومات مثل:</p>
+                                    <ol class="includes d-flex flex-wrap">
+                                        <li>وصف الخدمة</li>
+                                        <li>معلومات محددة</li>
+                                        <li>ملفات ذات صلة</li>
+                                        <li>الميزانية</li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="message-wrapper d-flex flex-column ">
+                                <textarea name="" class="form-control" placeholder="اكتب رسالتك"></textarea>
+                                {{-- <div class="attachments d-flex align-items-center flex-wrap">
+                                    <label>المرفقات:</label>
+                                    <div class="attachment">
+                                        <i class="fas fa-paperclip"></i>
+                                        <span>image.png</span>
+                                        <a href="">
+                                            <i class="fas fa-times"></i>
+                                        </a>
+                                    </div>
+                                    <div class="attachment">
+                                        <i class="fas fa-paperclip"></i>
+                                        <span>File.docx</span>
+                                        <a href="">
+                                            <i class="fas fa-times"></i>
+                                        </a>
+                                    </div>
+                                </div> --}}
+                                <div class="message-controls d-flex align-items-center">
+                                    <p class="counter">
+                                        <span class="current">87</span>
+                                        <span class="total">/ 2500</span>
+                                    </p>
+                                    <button type="button" class="attachment-btn"><i class="fas fa-paperclip"></i></button>
+                                    <button type="button" class="btn btn-yallow">ارسل</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- send-message modal -->
+
 @endsection
 
 

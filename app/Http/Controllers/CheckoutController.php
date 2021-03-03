ca5ffe49752d0e498070edce221c5580b6203f9c
MoaaidJamal <moaaid1998@gmail.com>
2021-03-04 00:09:24 +0200

    4/3 1 Muayad

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -142,9 +142,8 @@ class CheckoutController extends Controller
         $upload = new UploaderController();
         $upload->folder = 'orders';
         $upload->thumbFolder = 'orders/thumbs';
-        $galleryItemResponse = $upload->uploadSingle($q->requirements_attachments,false);
-
-        $Order->requirements_attachments = $galleryItemResponse['path'];;
+        $galleryItemResponse = $q->hasFile('requirements_attachments') ? $upload->uploadSingle($q->requirements_attachments,false) : null;
+        $Order->requirements_attachments = $galleryItemResponse ? $galleryItemResponse['path'] : null;
         $Order->save();
 
 
